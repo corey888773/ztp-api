30291981f9c3290f9876ceea90cc5ad14146380a
piotr-gasiorek <piotrga@remitly.com>
2025-03-04 20:32:06 +0100

    dummy server + mongo db connection

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"time"
+
+	"github.com/corey888773/ztp-api/srv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://ztp-mongo:27017"))
+	if err != nil {
+		panic(err)
+	}
+
+	server := srv.NewServer()
+	server.SetupRouter()
+	server.SetupDatabase(mongoClient)
+	err = server.Start(":8000")
+	if err != nil {
+		panic(err)
+	}
 }
